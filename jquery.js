f3d1f32cf8856dd442a4b46f2f0681a998b8f4c0
Alexander <xrocketmanx007@gmail.com>
2016-04-06 16:20:47 +0300

    added style manipulations, timer, and fade, slide, hide animations

--- jquery.js
+++ jquery.js
@@ -84,18 +84,320 @@ function jQuery(elements) {
 	Timer Functions
 	*************************
 	************************/
-	this.Hide = function() {
+
+	this.hide = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getNormalPrefix);
+		var opacityDeltas = getStyleDeltas('opacity', '0', count, getNormalPrefix);
+		changeStyleAll('overflow', 'hidden');
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+				addStyleValue(elements[i], 'opacity', opacityDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				opacity: '',
+				display: 'none',
+				overflow: ''
+			});
+			if (callback !== undefined) callback();
+		});
 		return this;
+	};
+
+	this.show = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getReversedPrefix);
+		var opacityDeltas = getStyleDeltas('opacity', '0', count, getReversedPrefix);
+		for (var i = 0; i < length; i++) {
+			setHeight(elements[i], '0px');
+		}
+		changeStyleAll({
+			opacity: '0',
+			display: '',
+			overflow: 'hidden'
+		});
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+				addStyleValue(elements[i], 'opacity', opacityDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				opacity: '',
+				overflow: ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.toggle = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getDisplayBasedPrefix);
+		var opacityDeltas = getStyleDeltas('opacity', '0', count, getDisplayBasedPrefix);
+		changeStyleAll('overflow', 'hidden');
+		for (var i = 0; i < length; i++) {
+			if (getStyleValue(elements[i], 'display') === 'none') {
+				setStyle(elements[i], 'display', parseStyleValue(''));
+				setStyle(elements[i], 'opacity', parseStyleValue('0'));
+				setHeight(elements[i], '0px');
+			}
+		}
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+				addStyleValue(elements[i], 'opacity', opacityDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				if (getStyleValue(elements[i], 'opacity') === '0') {
+					setStyle(elements[i], 'display', parseStyleValue('none'));
+				}
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				opacity: '',
+				'overflow': ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.fadeToggle = function(speed, callback) {
+		var count = getCount(speed);
+		var opacityDeltas = getStyleDeltas('opacity', '0', count, getDisplayBasedPrefix);
+		for (var i = 0; i < length; i++) {
+			if (getStyleValue(elements[i], 'display') === 'none') {
+				setStyle(elements[i], 'display', parseStyleValue(''));
+				setStyle(elements[i], 'opacity', parseStyleValue('0'));
+			}
+		}
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addStyleValue(elements[i], 'opacity', opacityDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				if (getStyleValue(elements[i], 'opacity') === '0') {
+					setStyle(elements[i], 'display', parseStyleValue('none'));
+				}
+			}
+			changeStyleAll('opacity', '');
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.fadeOut = function(speed, callback) {
+		this.fadeTo(speed, '0', function() {
+			changeStyleAll({
+				display: 'none',
+				opacity: ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.fadeIn = function(speed, callback) {
+		changeStyleAll({
+			display: '',
+			opacity: '0'
+		});
+		this.fadeTo(speed, '1', function() {
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.fadeTo = function(speed, opacity, callback) {
+		var count = getCount(speed);
+		var opacityDeltas = getStyleDeltas('opacity', opacity, count, getNormalPrefix);
+		changeStyleAll('display', '');
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addStyleValue(elements[i], 'opacity', opacityDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.slideToggle = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getDisplayBasedPrefix);
+		changeStyleAll('overflow', 'hidden');
+		for (var i = 0; i < length; i++) {
+			if (getStyleValue(elements[i], 'display') === 'none') {
+				setStyle(elements[i], 'display', parseStyleValue(''));
+				setHeight(elements[i], '0px');
+			}
+		}
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				if (getStyleValue(elements[i], 'height') === '0px') {
+					setStyle(elements[i], 'display', parseStyleValue('none'));
+				}
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				overflow: ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.slideUp = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getNormalPrefix);
+		changeStyleAll('overflow', 'hidden');
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				display: 'none',
+				overflow: ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	this.slideDown = function(speed, callback) {
+		var count = getCount(speed);
+		var heightDeltas = getHeightDeltas('0px', count, getReversedPrefix);
+		for (var i = 0; i < length; i++) {
+			setHeight(elements[i], '0px');
+		}
+		changeStyleAll({
+			overflow: 'hidden',
+			display: ''
+		});
+		Timer(function() {
+			for (var i = 0; i < length; i++) {
+				addSize(elements[i], heightDeltas[i]);
+			}
+		}, 
+		count, 
+		function() {
+			for (var i = 0; i < length; i++) {
+				setHeight(elements[i], '');
+			}
+			changeStyleAll({
+				overflow: ''
+			});
+			if (callback !== undefined) callback();
+		});
+		return this;
+	};
+
+	var timeout = 10;
+	function getCount(speed) {
+		if (speed !== undefined) {
+			return speed / timeout;
+		}
+		return 1;
 	}
 
-	function Timer(func, speed, callback) {
-		var timeout = 10;
-		var count = speed / timeout;
+	function getNormalPrefix(element, currentStyle, targetStyle) {
+		return currentStyle.value > targetStyle.value? "-=" : "+=";
+	}
+
+	function getReversedPrefix(element, currentStyle, targetStyle) {
+		return currentStyle.value < targetStyle.value? "-=" : "+=";
+	}
+
+	function getDisplayBasedPrefix(element, currentStyle, targetStyle) {
+		if (getStyleValue(element, 'display') !== 'none') {
+			return getNormalPrefix(element, currentStyle, targetStyle);
+		} else {
+			return getReversedPrefix(element, currentStyle, targetStyle);
+		}
+	}
+
+	function addSize(element, sizeDelta) {
+		for (var key in sizeDelta) {
+			addStyleValue(element, key, sizeDelta[key]);
+		}
+	}
+
+	function setHeight(element, size) {
+		setStyle(element, 'height', parseStyleValue(size));
+		setStyle(element, 'paddingTop', parseStyleValue(size));
+		setStyle(element, 'paddingBottom', parseStyleValue(size));
+	}
+
+	function getHeightDeltas(targetValue, count, getPrefix) {
+		var deltas = [];
+		var heightDeltas = getStyleDeltas('height', targetValue, count, getPrefix);
+		var paddingTopDeltas = getStyleDeltas('paddingTop', targetValue, count, getPrefix);
+		var paddingBottomDeltas = getStyleDeltas('paddingBottom', targetValue, count, getPrefix);
+		for (var i = 0; i < length; i++) {
+			var delta = {};
+			delta.height = heightDeltas[i];
+			delta.paddingTop = paddingTopDeltas[i];
+			delta.paddingBottom = paddingBottomDeltas[i];
+			deltas.push(delta);
+		}
+		return deltas;
+	}
+
+	function getStyleDeltas(style, targetValue, count, getPrefix) {
+		var deltas = [];
+		var currentStyle;
+		var prefix;
+		var targetStyle = parseStyleValue(targetValue);
+		for (var i = 0; i < length; i++) {
+			currentStyle = parseStyleValue(getStyleValue(elements[i], style));
+			prefix = getPrefix(elements[i], currentStyle, targetStyle);
+			var value = prefix + 
+				(Math.abs(currentStyle.value - targetStyle.value) / count) + 
+				targetStyle.measure;
+			deltas.push(parseStyleValue(value));
+		}
+		return deltas;
+	}
+
+	function Timer(func, count, callback) {
 		var interval = setInterval(function() {
 			func();
-			if (count < 1) {
+			if (count <= 1) {
 				clearInterval(interval);
-				callback();
+				if (callback !== undefined) callback();
 			} else {
 				count--;
 			}
@@ -104,35 +406,87 @@ function jQuery(elements) {
 
 	/************************
 	*************************
-	Dom Manipulating
+	Style Manipulating
 	*************************
 	************************/
-	//refactor:
 	this.css = function(style, value) {
-		changeStyle(style, value);
+		if(isCssRequest(style, value)) {
+			return getStyleValue(elements[0], style);
+		} else {
+			changeStyleAll(style, value);
+			return this;
+		}
 	};
 
-	function changeStyle(style, value) {
+	function isCssRequest(style, value) {
+		return typeof style === "string" && value === undefined;
+	}
+
+	function changeStyleAll(style, value) {
 		if (typeof style === "string") {
-			if (value.indexOf("+=") === 0) {
-				var delta = value.slice(2, value.length - 2);
+			var styleValue = parseStyleValue(value);
+			if (styleValue.adding) {
 				for (var i = 0; i < length; i++) {
-					var oldStyle = elements[i].style[style];
-					var oldValue = oldStyle.slice(0, oldStyle.length - 2);
-					elements[i].style[style] = Number(oldValue) + Number(delta) + "px";
-					alert(elements[i].style[style]);
+					addStyleValue(elements[i], style, styleValue);
 				}
 			} else {
 				for (var i = 0; i < length; i++) {
-					elements[i].style[style] = value;
+					setStyle(elements[i], style, styleValue);
 				}
 			}
 		} else if (typeof style === "object") {
 			for (var i = 0; i < length; i++) {
 				for (var key in style) {
-					elements[i].style[key] = style[key];
+					var styleValue = parseStyleValue(style[key]);
+					if (styleValue.adding) {
+						addStyleValue(elements[i], key, styleValue);
+					} else {
+						setStyle(elements[i], key, styleValue);
+					}
 				} 			
 			}
 		}
+	}
+
+	function addStyleValue(element, style, styleValue) {
+		var oldValue = parseStyleValue(getStyleValue(element, style));
+		var newValue = oldValue.value;
+		if (styleValue.sign === '+') {
+			newValue += styleValue.value;
+		} else {
+			newValue -= styleValue.value;
+		}
+		element.style[style] = newValue + styleValue.measure;
+	}
+
+	function setStyle(element, style, styleValue) {
+		element.style[style] = styleValue.fullValue;
+	}
+
+	function getStyleValue(element, style) {
+		return getComputedStyle(element)[style];
+	}
+
+	function parseStyleValue(value) {
+		var result = {};
+		result.adding = value.indexOf("=") >= 0;
+		if (result.adding) {
+			result.sign = value[0];
+			value = value.slice(2);
+		}
+		result.fullValue = value;
+		for (var i = value.length - 1; i >= 0; i--) {
+			if (!isNaN(value[i])) {
+				i++;
+				break;
+			}
+		}
+		result.measure = "";
+		if (i < value.length) {
+			result.measure = value.slice(i);
+			value = value.slice(0, i);
+		}
+		result.value = Number(value);
+		return result;
 	}
 }
